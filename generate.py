930a3dc8ffdd7d158559064c08bbfc065cf090df
Jonah Pears <jonahpears@gmail.com>
2022-12-21 19:36:45 +0000

    vars for box position

--- generate.py
+++ generate.py
@@ -2,10 +2,9 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
 
-length = 1
-width = 1
-height = 1
+length, width, height = [1, 2, 3]
+x, y, z = [0, 0, 0.5 + height]
 
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[length, width, height])
+pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
 
 pyrosim.End()
