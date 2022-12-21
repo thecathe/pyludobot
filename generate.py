a001735f1852ad03ccb62064b59bc530258c3661
Jonah Pears <jonahpears@gmail.com>
2022-12-21 17:01:47 +0000

    fixed generate import

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
-import pyrosim.pyrosim
+import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
 
 pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
 
-pyrosim.end()
+pyrosim.End()
