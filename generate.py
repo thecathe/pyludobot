c809e6b3705de723bf8c2d2eb648a5e5b6b6e1fd
Jonah Pears <jonahpears@gmail.com>
2022-12-21 20:27:54 +0000

    many tower stack

--- generate.py
+++ generate.py
@@ -2,18 +2,28 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("boxes.sdf")
 
-length, width, height = [1, 2, 3]
-x, y, z = [0, 0, 0.5 + height]
+start_length, start_width, start_height = [1, 1, 1]
+length, width, height = [start_length, start_width, start_height]
 
-# pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
-# pyrosim.Send_Cube(name="Box2", pos=[x, width+y, height+z], size=[
-#                   length, width, height])
+z_sep = 0.1
+origin_x, origin_y, origin_z = [0, 0, start_height + z_sep]
 
-for i in range(0, 10):
-    pyrosim.Send_Cube(name=f"Box{i}",
-                      pos=[x, y, z+(height*i)],
-                      size=[length, width, height]
-                      )
+for xi in range(0, 2):
+    for yi in range(0, 2):
+        z_offset = 0.1
+        length, width, height = [1, 1, 1]
+        x, y, z = [0+(xi*width)+((start_width/2)-(width/2)),
+                   0 + (yi*length)+((start_length/2)-(length/2)),
+                   origin_z + height]
+        for i in range(0, 10):
+            pyrosim.Send_Cube(name=f"Box{i}",
+                              pos=[x, y, z+z_offset],
+                              size=[length, width, height]
+                              )
+            z_offset += z_sep+height
+            length *= 0.9
+            width *= 0.9
+            height *= 0.9
 
 
 pyrosim.End()
