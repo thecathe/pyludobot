4bb8a2850747d5ab98a226cbf987a9b97b166317
Jonah Pears <jonahpears@gmail.com>
2022-12-21 19:37:06 +0000

    multiple boxes

--- generate.py
+++ generate.py
@@ -1,10 +1,11 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
 length, width, height = [1, 2, 3]
 x, y, z = [0, 0, 0.5 + height]
 
 pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+pyrosim.Send_Cube(name="Box2", pos=[x, y, z], size=[length, width, height])
 
 pyrosim.End()
