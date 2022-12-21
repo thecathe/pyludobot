8338026946643ed8cc9e4f286809fe369a0048a1
Jonah Pears <jonahpears@gmail.com>
2022-12-21 19:41:45 +0000

    moved box position to be staggered

--- generate.py
+++ generate.py
@@ -6,6 +6,7 @@ length, width, height = [1, 2, 3]
 x, y, z = [0, 0, 0.5 + height]
 
 pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
-pyrosim.Send_Cube(name="Box2", pos=[x, y, z], size=[length, width, height])
+pyrosim.Send_Cube(name="Box2", pos=[x, width+y, height+z], size=[
+                  length, width, height])
 
 pyrosim.End()
